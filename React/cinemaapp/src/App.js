b6f8112c64fdc7b1b7d424866b4f6d21c15acb75
James Monks <James.Monks@academytrainee.com>
2020-09-01 10:20:24 +0100

    started BrowserRouter

--- React/cinemaapp/src/App.js
+++ React/cinemaapp/src/App.js
@@ -6,11 +6,12 @@ import Contact from './contactform.js'
 import Seat from './SeatSelector.jsx'
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
       <div className="App">
         <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-          <a className="navbar-brand" href="IOHome.html"><img src="" alt="Logo" style={{ widows: "80px;px" }} href="IOHome.html" /></a>
+          <Link to="/">Home</Link>
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link" href="">Hello</a>
@@ -26,10 +27,7 @@ function App() {
       </header>
       </div>
     </div>
-
-
-
+</BrowserRouter>
   );
 }
-
 export default App;
