832706a97c37fb397d4faae67580c237a1d47912
James Monks <James.Monks@academytrainee.com>
2020-09-01 12:40:43 +0100

    started map script

--- React/cinemaapp/src/findUs.jsx
+++ React/cinemaapp/src/findUs.jsx
@@ -1,1 +1,23 @@
-import React from 'react';
+import React, { createRef } from 'react';
+
+class FindUs extends React.Component {
+   googleMapRef = createRef()
+
+   createGoogleMap = () => {
+       new window.google.maps.Map(this.googleMapRef.current, {
+           zoom: 16,
+           center: {
+               lat: 53.474005,
+               lng: -2.2879854
+           },
+           disableDefaultUI: true
+       })
+   }
+
+   render() {
+       return (
+           <div id="google-map" ref={this.googleMapRef} style={{width: '400px', height: '300px'}} />
+       )
+   }
+}
+export default FindUs;
