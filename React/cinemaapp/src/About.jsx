830ade9bed692dc05debe8f08f1c76da7b9bf1e0
Chynna <chyanna106@gmail.com>
2020-09-01 12:54:22 +0100

    preparing to pull work

--- React/cinemaapp/src/About.jsx
+++ React/cinemaapp/src/About.jsx
@@ -3,6 +3,38 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class About extends React.Component {
     
+    render() {
+        return(
+          <div class="text-center text-wrap">
+          <h2>About IO</h2>
+          <p>Welcome to IO Cinema. Our mission is to change the way we enjoy movies. All of our venues offer a unique viewing experience alike no other. Need to go to the toilet during the movie, no problem! Press the button located by your cup holder to alert one of our IO ushers. They will provide you with a tablet that is live streaming your movie, so you never miss a moment!</p>
+          </div>  
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="..." class="d-block w-100" alt="...">
+                        </img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                        </img>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                        </img>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+                </a>
+            </div>
+        );
+      }
     
 
     
--- React/cinemaapp/src/App.jsx
+++ React/cinemaapp/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
       <div className="App">
         <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-          <a className="navbar-brand" href="IOHome.html"><img src="/Ima" alt="Logo" style={{ widows: "80px;px" }} href="IOHome.html" /></a>
+          <a className="navbar-brand" href="IOHome."><img src="/Ima" alt="Logo" style={{ widows: "80px;px" }} href="IOHome.html" /></a>
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link" href="">Hello</a>
@@ -22,8 +22,8 @@ function App() {
         
         
       </div>
-      <Seat />
-      {/* <About/> */}
+      {/* <Seat /> */}
+      <About/>
 
     </div>
 
