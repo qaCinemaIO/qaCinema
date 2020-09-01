aa68d2152c3fa52165e88340ab021a1f16c40702
James Monks <James.Monks@academytrainee.com>
2020-09-01 10:45:18 +0100

    site navigation working

--- React/cinemaapp/src/App.js
+++ React/cinemaapp/src/App.js
@@ -2,31 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Contact from './contactform.js'
+import {BrowserRouter, Switch,Route, Link } from "react-router-dom"
 
 import Seat from './SeatSelector.jsx'
+
+function HomePage() {
+  return <h1>This is a placeholder for the Home page.</h1>
+}
+
+function SeatSelectPage() {
+  return <Seat />
+}
+
+function ContactPage() {
+  return <Contact />
+}
 function App() {
   return (
     <BrowserRouter>
-    <div>
+    
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
       <div className="App">
         <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-          <Link to="/">Home</Link>
+          <Link className="navbar-brand" to="/">Home</Link>
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="">Hello</a>
+              <Link className="navbar-brand" to="/seatselect">Select a Seat</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href=""></a>
+              <Link className="navbar-brand" to="/contact">Contact Us</Link>
             </li>
           </ul>
         </nav>
         
-        <header className="App-header">
-        <Contact />
-      </header>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/seatselect/" component={SeatSelectPage} />
+          <Route exact path="/contact/" component={ContactPage} />
+        </Switch>
+        
       </div>
-    </div>
+    
 </BrowserRouter>
   );
 }
