2e67dd28a4411265955dc85529518ed22509e5fe
James Monks <James.Monks@academytrainee.com>
2020-09-01 14:24:19 +0100

    working on map

--- React/cinemaapp/src/findUs.jsx
+++ React/cinemaapp/src/findUs.jsx
@@ -1,8 +1,8 @@
 import React, { createRef } from 'react';
 
 class FindUs extends React.Component {
-   googleMapRef = createRef()
-
+    googleMapRef = createRef()
+    
    createGoogleMap = () => {
        new window.google.maps.Map(this.googleMapRef.current, {
            zoom: 16,
@@ -11,6 +11,17 @@ class FindUs extends React.Component {
                lng: -2.2879854
            },
            disableDefaultUI: true
+       })
+   }
+
+   componentDidMount() {
+       const googleMapScript = document.createElement('script')
+
+       googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCDQ3GLDoRqdKEgq8tb7Drm8BJ9BN2cWwM&libraries=places`
+       window.document.body.appendChild(googleMapScript)
+
+       googleMapScript.addEventListener('load', {
+           googleMapRef : this.createGoogleMap()
        })
    }
 
