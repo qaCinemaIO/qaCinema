6ec4958f3a7f3230ee6195ba72d7b9f7387438de
Suf-dev <Sufyaan.Aziz@qa.com>
2020-09-01 12:31:39 +0100

    backend updated added CRUD Functionality

---
+++ Express/models/movie.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const movieSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: true
+    },
+    tech: {
+        type: String,
+        required: true
+    },
+    released: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+
+})
+
+module.exports = mongoose.model('Movie', movieSchema)
