4c1c331fe9a3454723b40c17d9836f1e6128a837
gianluca-fernandez-QA <gianluca.fernandez@academytrainee.com>
2020-09-02 14:51:19 +0100

    IQC-106 #comment added cors

--- React/cinemaapp/src/SeatSelector.jsx
+++ React/cinemaapp/src/SeatSelector.jsx
@@ -5,6 +5,7 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { Table } from 'reactstrap';
 import { createPortal } from 'react-dom';
 import Seat from './Seat';
+import axios from 'axios';
 
 let SeatRow = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
 
@@ -26,11 +27,15 @@ class Example extends React.Component {
     }
     createSomething = () => {
         let seatTable = [];
+        let fetchplace;
+        SeatUpdate(fetchplace);
         for (let i = 0; i < SeatRow.length; i++) {
+            
             let seats = [];
             let b = SeatRow[i];
             for (let a = 1; a <= 20; a++) {
-                seats.push(<td><Seat row={b} num={a} /></td>)
+                seats.push(<td><Seat row={b} num={a} seatd={SeatUpdate(fetchplace)}/></td>)
+                fetchplace++;
             }
             seatTable.push(<tr>{seats}</tr>)
         }
@@ -45,7 +50,7 @@ class Example extends React.Component {
                     <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
                     <ModalBody>
 
-                        <Table dark>
+                        <Table dark responsive>
                             <tbody>
                                 {this.createSomething()}
                             </tbody>
@@ -61,5 +66,14 @@ class Example extends React.Component {
     }
 
 }
-
+function SeatUpdate(i){
+    axios.get('http://localhost:9007/info')
+  .then(function (response) {
+      console.log(response.data[i]);
+    return response.data[i];
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
 export default Example;
