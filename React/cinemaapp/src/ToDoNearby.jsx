fb331f555988ffddd20c4562d1db9a77df437237
James Monks <James.Monks@academytrainee.com>
2020-09-04 11:14:20 +0100

    things to do page done

--- React/cinemaapp/src/ToDoNearby.jsx
+++ React/cinemaapp/src/ToDoNearby.jsx
@@ -5,7 +5,7 @@ class NearbyPlaces extends React.Component {
     render() {
         return(
             <div className="container">
-                <center><h1>Things to do nearby</h1></center>
+                <center><h1>Things to do nearby</h1></center><br></br>
                 <div className="row">
                     <div className = "col-md">
                         <h3>Restaurants</h3>
@@ -23,12 +23,60 @@ class NearbyPlaces extends React.Component {
                         <p><strong>Tandoori Delights</strong> - Indian restaurant<br></br>
                         Salford M50 3AY <br></br>
                           <a href="https://www.quaysidemediacityuk.co.uk/food-and-drink/tandoori-delights&CategoryID=110">Tandoori Delights</a></p>
+                        <p><strong>Wagamama</strong> - Asian & Japanese Restaurant<br></br>
+                        Unit 2 orange tower media city, Salford M50 2EQ<br></br>
+                          <a href="https://www.wagamama.com/restaurants/media-city">wagamama</a></p>
+                        <p><strong>Nando's Salford Quays</strong> - Chicken restaurant<br></br>
+                        Lowry Plaza, The Quays Unit 2, Digital World Centre, Salford M50 3UB <br></br>
+                          <a href="https://www.nandos.co.uk/eat/restaurants/salford-quays?utm_source=google&utm_medium=mybusiness&utm_campaign=restaurant-page&utm_term=nandossalfordquays">Nando's Salford Quays</a></p>
+                        <p><strong>Lime</strong> - Bar & grill<br></br>
+                        Unit G1, The Quays, Salford M50 3AG <br></br>
+                          <a href="http://www.limeuk.com/">Lime</a></p>
+                        <p><strong>Bella Italia</strong> - Italian Restaurant<br></br>
+                        The Lowry, Unit G40a, Manchester M50 3AZ <br></br>
+                          <a href="https://www.bellaitalia.co.uk/italian-restaurant/manchester/salford-quays">Bella Italia</a></p>
+                        <p><strong>Pier Eight Restaurant</strong> - Modern British restaurant<br></br>
+                        Inside The Lowry, Salford M50 3AZ <br></br>
+                          <a href="http://www.piereightrestaurant.co.uk/">Pier Eight Restaurant</a></p>
+                        <p><strong>Pizza Express</strong> - Pizza Restaurant<br></br>
+                        G38, The Lowry Designer Outlet the Quays, The Quays, Salford M50 3AZ <br></br>
+                        <a href="https://www.pizzaexpress.com/salford-quays?utm_source=google&utm_medium=Places&utm_campaign=salford-quays">Pizza Express</a></p>
                     </div>
                     <div className="col-md">
-                        <h3>Bars</h3>
+                        <h3>Bars & Pubs</h3>
+                        There are a number of fine bars nearby where you can cheerfully sip a cocktail or a pint, whatver may take your fancy!<br></br><br></br>
+                        <p><strong>The Botanist Media City</strong> - Bar<br></br>
+                        - Orange Building MediaCityUK, Salford M50 2HE <br></br>
+                          <a href="http://thebotanist.uk.com/locations/mediacityuk">The Botanist Media City</a></p>
+                        <p><strong>The Dockyard</strong> - Gastropub<br></br>
+                        - DockHouse, Media City, Salford M50 2EQ <br></br>
+                          <a href="http://dockyard.pub/">The Dockyard</a></p>
+                        <p><strong>The Alchemist</strong> - Cocktail bar<br></br>
+                        - The Bund, The Quays, MediaCityUK, Salford M50 3AB <br></br>
+                          <a href="https://thealchemist.uk.com/venues/mediacity-uk-manchester/">The Alchemist</a></p>
+                        <p><strong>Craftbrew</strong> - Bar<br></br>
+                        - Unit 1, Digital World Centre, Lowry Plaza, The Quays, Salford M50 3UB <br></br>
+                          <a href="http://craftbrew-uk.com/">Craftbrew</a></p>
                     </div>
                     <div className="col-md">
                         <h3>Others</h3>
+                        There is all sorts to do around the quays, why not make a day of it at some of these attractions? <br></br><br></br>
+                        <p><strong>The Lowry</strong> - Arts/entertainment centre with 2 theatres plus galleries<br></br>
+                        - Pier, 8 The Quays, Salford M50 3AZ <br></br>
+                          <a href="https://www.thelowry.com/">The Lowry</a></p>
+                        <p><strong>Helly Hansen Watersports Centre</strong> - The North West’s premier watersports centre<br></br>
+                        - 15 The Quays, Salford M50 3SQ <br></br>
+                          <a href="http://www.salfordwatersports.com/">Helly Hansen Watersports Centre</a></p>
+                        <p><strong>BBC Tours at MediaCityUK</strong> - Walking Tours of BBC studios<br></br>
+                        - Dock House, The Quays, Salford M50 2LH <br></br>
+                          <a href="https://www.bbc.co.uk/showsandtours/tours/bbc-tour-at-mediacityuk">BBC Tours at MediaCityUK</a></p>
+                        <p><strong>Coronation Street The Tour</strong> - Wander the cobbles of the famous Coronation Street on this tour of its original filming location.<br></br>
+                        - MEDIACITYUK, THE STUDIOS FROM DOCK10, Salford M50 2EQ <br></br>
+                          <a href="https://www.itv.com/coronationstreettour/">Coronation Street The Tour</a></p>
+                        <p><strong>Imperial War Museum North</strong> - War museum<br></br>
+                        - Trafford Wharf Rd, Trafford Park, Stretford, Manchester M17 1TZ <br></br>
+                          <a href="http://www.iwm.org.uk/visits/iwm-north">IWM North</a></p>
+
                     </div>
                 </div>
 
